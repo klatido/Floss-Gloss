2709a9cbb8c56f3200aed053e944abe8ed1ecee5
klatido <ken_latido@dlsu.edu.ph>
2026-03-17 16:04:35 +0800

    .

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $password = "";
 $database = "floss_gloss_dental";
+$port = 3307;
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include("config/database.php");
 
 echo "<h1>Floss & Gloss Dental</h1>";
