bc1ebf20143fb1274e163cf8544e70a482d2a403
klatido <ken_latido@dlsu.edu.ph>
2026-03-17 16:20:27 +0800

    .

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "HELLO FLOSS & GLOSS";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include("config/database.php");
+
+echo "<h1>Floss & Gloss Dental</h1>";
+echo "<p>Database connected successfully!</p>";
 ?>
